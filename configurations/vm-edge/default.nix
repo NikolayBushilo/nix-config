5e9ed402b88804d43c4bfc3d485104a990128a8e
Nikolay Bushilo <github@bushilo.com>
2026-03-10 04:48:59 +0100

    Fixed a typo

--- configurations/vm-edge/default.nix
+++ configurations/vm-edge/default.nix
@@ -173,7 +173,11 @@ in {
         settings.PasswordAuthentication = false;
 
         hostKeys = [
-            "/data/identities/ssh_host_ed25519_key"
+            {
+                path = "/data/identities/ssh_host_ed25519_key";
+                type = "ed25519";
+            }
+            
         #    {
         #        path = config.sops.secrets.ssh_host_ed25519_key.path;
         #        type = "ed25519";
